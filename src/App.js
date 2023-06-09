9fb322cae2bc12104967aeec3ecf9342981eef34
Akshay Saini <akshaymarch7@gmail.com>
2023-06-09 08:27:59 +0530

    episode-07

--- src/App.js
+++ src/App.js
@@ -2,28 +2,11 @@ import React, { lazy, Suspense, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-//import About from "./components/About";
+import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import UserContext from "./utils/UserContext";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore";
-import Cart from "./components/Cart";
-
-//import Grocery from "./components/Grocery";
-
-// Chunking
-// Code Splitting
-// Dynamic Bundling
-// lazy Loading
-// on demand loading
-// dynamix imoprt
-
-const Grocery = lazy(() => import("./components/Grocery"));
-
-const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
   const [userName, setUserName] = useState();
@@ -38,14 +21,10 @@ const AppLayout = () => {
   }, []);
 
   return (
-    <Provider store={appStore}>
-      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-        <div className="app">
-          <Header />
-          <Outlet />
-        </div>
-      </UserContext.Provider>
-    </Provider>
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
   );
 };
 
@@ -60,31 +39,15 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: (
-          <Suspense fallback={<h1>Loading....</h1>}>
-            <About />
-          </Suspense>
-        ),
+        element: <About />,
       },
       {
         path: "/contact",
         element: <Contact />,
       },
       {
-        path: "/grocery",
-        element: (
-          <Suspense fallback={<h1>Loading....</h1>}>
-            <Grocery />
-          </Suspense>
-        ),
-      },
-      {
         path: "/restaurants/:resId",
         element: <RestaurantMenu />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
       },
     ],
     errorElement: <Error />,
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,17 +1,22 @@
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import useRestaurantMenu from "../utils/useRestrauntMenu";
-import RestaurantCategory from "./RestaurantCategory";
-import { useState } from "react";
+import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
   const { resId } = useParams();
 
-  const dummy = "Dummy Data";
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
-  const resInfo = useRestaurantMenu(resId);
-
-  const [showIndex, setShowIndex] = useState(null);
+  const fetchMenu = async () => {
+    const data = await fetch(MENU_API + resId);
+    const json = await data.json();
+    setResInfo(json.data);
+  };
 
   if (resInfo === null) return <Shimmer />;
 
@@ -21,31 +26,23 @@ const RestaurantMenu = () => {
   const { itemCards } =
     resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
-  const categories =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
-      (c) =>
-        c.card?.["card"]?.["@type"] ===
-        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
-    );
-  //console.log(categories);
+  console.log(itemCards);
 
   return (
-    <div className="text-center">
-      <h1 className="font-bold my-6 text-2xl">{name}</h1>
-      <p className="font-bold text-lg">
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
-      {/* categories accordions */}
-      {categories.map((category, index) => (
-        // controlled component
-        <RestaurantCategory
-          key={category?.card?.card.title}
-          data={category?.card?.card}
-          showItems={index === showIndex ? true : false}
-          setShowIndex={() => setShowIndex(index)}
-          dummy={dummy}
-        />
-      ))}
+      <h2>Menu</h2>
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} -{" Rs."}
+            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
